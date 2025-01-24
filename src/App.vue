3e7e6157922880aa37bd8b3053835566d82ae120
AndreaMargutti <marguttiandrea30@gmail.com>
2025-01-24 13:02:11 +0100

    start repo

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+  @use  '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+  @use './style/general.scss' as *;
+</style>

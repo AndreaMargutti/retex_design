141c7c4cecb90383a5c1962dc1fe6d78ac73faf7
AndreaMargutti <marguttiandrea30@gmail.com>
2025-01-27 15:46:07 +0100

    removed btn

--- src/components/header/AppNavbar.vue
+++ src/components/header/AppNavbar.vue
@@ -25,16 +25,14 @@ export default {
   <!-- sezione menu -->
   <section class="d-flex justify-content-between">
     <div class="p-3 border-div">
-      <button v-on:click="openMenu">
          <!-- icona hamburger menu -->
-         <svg v-show="list" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" color="red">
+         <svg v-show="list" v-on:click="openMenu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" color="red">
            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
           </svg>
           <!-- icona X -->
-          <svg v-show="!list" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" color="red">
+          <svg v-show="!list" v-on:click="openMenu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" color="red">
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
           </svg>
-      </button>
     </div>
     
     <div class="p-3 border-div">

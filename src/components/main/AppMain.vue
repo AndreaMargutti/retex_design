435ee25e1ddc0c0ac8cc3f23bc352ecef92044ed
AndreaMargutti <marguttiandrea30@gmail.com>
2025-01-27 18:54:23 +0100

    second article

--- src/components/main/AppMain.vue
+++ src/components/main/AppMain.vue
@@ -1,5 +1,7 @@
 <script>
 import AppCTA from './AppCTA.vue';
+import MainArticle from './MainArticle.vue';
+import ThreeArticle from './ThreeArticle.vue';
 
 export default {
   data() {
@@ -9,68 +11,21 @@ export default {
   },
 
   components: {
+    MainArticle,
+    ThreeArticle,
     AppCTA,
   }
 }
 </script>
 
 <template>
-  <section>
-    <div class="d-flex flex-column justify-content-end" id="main">
-      <p class="tag">MIGRANTI</p>
-      <h1>
-        Roccella Jonica, la Lampedusa che  <br>
-        l'Italia ignora
-      </h1>
-      <figure class="d-flex">
-        <img src="../../../public/e059a7a3367ff920d07e1938b4e8cd8f.jpeg" alt="profile-pic">
-        <div>
-          <h5>Di Alessandro Puglia</h5>
-          <h6>29 giugno 2022</h6>
-        </div>
-      </figure>
-    </div>
-  </section>
-
+  <MainArticle />
+  <ThreeArticle />
   <!-- Inserisco componente CTA -->
   <AppCTA />
 
 </template>
 
 <style scoped lang="scss">
-  section {
-    border: 0;
-  }
 
-  #main {
-    height: 700px;
-    background-image: url(../../../public/44146596a39e532d863a717f24411c63.jpeg);
-    background-size: cover;
-    padding: 1.2rem;
-  }
-
-  .tag {
-    background-color: white;
-    width: fit-content;
-    padding: 10px;
-    border: 2px solid black;
-    border-radius: 10px;
-    box-shadow: -3px 3px;
-  }
-
-  h1 {
-    color: white;
-    font-weight: 600;
-  }
-  
-
-  figure {
-    color: white;
-    margin-top: 25px;
-    img {
-      height: 50px;
-      border-radius: 50%;
-      margin-right: 25px;
-    }
-  }
 </style>
---
+++ src/components/main/ThreeArticle.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  data() {
+    return {
+        name: 'ThreeArticle', 
+    }
+  }
+}
+</script>
+
+<template>
+    <section>
+        <div id="long-article" class="d-flex flex-column justify-content-end">
+            <p class="tag">ALLARMI</p>
+            <h1>
+                Il collasso dei ghiacciai negli scatti <br> 
+                del fotografo ambientale Fabiano Ventura
+            </h1>
+            <figure>
+                <img src="../../../public/d7312632d8e9cc247b26cdd7abe33d50.png" alt="">
+                <div>
+                    <h5>Di Ugo Lombi</h5>
+                    <h6>14 Luglio 2022</h6>
+                </div>
+            </figure>
+        </div>
+        <div></div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+#long-article {
+    min-height: 700px;
+    background-image: url(../../../public/6b12b5889d6ddad962a628409ed1fb35.png);
+    background-size: cover;
+    padding: 1.2rem;
+}
+
+.tag {
+background-color: #e2f1e8;
+width: fit-content;
+padding: 10px;
+border: 2px solid black;
+border-radius: 10px;
+box-shadow: -3px 3px;
+}
+
+h1 {
+    color: white;
+    font-weight: 600;
+}
+
+figure {
+    color: white;
+    margin-top: 25px;
+    img {
+      height: 50px;
+      border-radius: 50%;
+      margin-right: 25px;
+    }
+  }
+
+</style>

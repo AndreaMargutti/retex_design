102b50458c8b9fc5b7aa41820c9a69f0f2283b59
AndreaMargutti <marguttiandrea30@gmail.com>
2025-01-27 14:36:54 +0100

    add show-div

--- src/components/header/AppNavbar.vue
+++ src/components/header/AppNavbar.vue
@@ -2,13 +2,21 @@
 export default {
   data() {
     return {
-        name: 'AppNavbar', 
+        name: 'AppNavbar',
+        list: true,
     }
   },
 
   props: {
     navItems: Array
-  }
+  },
+
+  methods: {
+     openMenu () {
+        //inserisco o tolgo classe d-none
+        this.list = !this.list;
+      }
+  },
 }
 </script>
 
@@ -17,11 +25,18 @@ export default {
   <!-- sezione menu -->
   <section class="d-flex justify-content-between">
     <div class="p-3 border-div">
-      <!-- icona hamburger menu -->
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" color="red">
-        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-      </svg>
+      <button v-on:click="openMenu">
+         <!-- icona hamburger menu -->
+         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" color="red">
+           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+          </svg>
+          <!-- icona X -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x d-none" viewBox="0 0 16 16" color="red">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+          </svg>
+      </button>
     </div>
+    
     <div class="p-3 border-div">
       <!-- icona lente -->
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16" color="red">
@@ -31,8 +46,8 @@ export default {
   </section>
 
   <!-- sezione lista -->
-
-  <section class=" d-flex justify-content-between">
+<div v-show="list">
+  <section v-show="list" class="d-flex justify-content-between">
     <ul class="d-flex gap-3">
       <li v-for="item in navItems">
         <h3>{{ item }}</h3>
@@ -47,6 +62,7 @@ export default {
       </svg>
     </div>
   </section>
+</div>
 
 </template>
 
